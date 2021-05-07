fb4ca2bb577df7c1a1e2b845925117c588cb4dce
rojoMojoGit <sheickitout@gmail.com>
2021-05-07 04:04:41 +0000

    mvc

--- imageDetectionWithARKit/ViewController.swift
+++ imageDetectionWithARKit/ViewController.swift
@@ -16,27 +16,80 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sceneView.autoenablesDefaultLighting = true
+        
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
+   // Image Detection -> Anchor - 10 images
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+       
+        // Optionals: if-let or guard-let
+        
+        // Link to the ARResource Group
+        if let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+            
+            // Tracking
+            configuration.detectionImages = referenceImages
+            configuration.maximumNumberOfTrackedImages = 2
+            
+            print("Images have been successfully loaded.")
+        }
 
         // Run the view's session
         sceneView.session.run(configuration)
+    }
+    
+    // Detection
+    func renderer(_ render: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
+        let node = SCNNode()
+        
+        if let anchor = anchor as? ARImageAnchor {
+            let plane = SCNPlane(width: anchor.referenceImage.physicalSize.width, height: anchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1, alpha: 0.5)
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+            
+            if anchor.referenceImage.name == "eevee-card" {
+                
+                // Root Node -> Plane Node -> Model Node
+                
+                if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        pokeNode.eulerAngles.x = .pi / 2
+                        
+                        planeNode.addChildNode(pokeNode)
+                    }
+                }
+            }
+            
+            if anchor.referenceImage.name == "oddish-card" {
+                
+                // Root Node -> Plane Node -> Model Node
+                
+                if let pokeScene = SCNScene(named: "art.scnassets/oddish.scn") {
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        pokeNode.eulerAngles.x = .pi / 2
+                        
+                        planeNode.addChildNode(pokeNode)
+                    }
+                }
+            }
+        }
+        
+        return node
     }
     
     override func viewWillDisappear(_ animated: Bool) {
